54d6c5846b24ac31f1d5a1d9d273dc23cfb6f94c
alijandrobalana <76250929+alijandrobalana@users.noreply.github.com>
2021-01-18 21:48:20 +0800

    Add files via upload

---
+++ database/migrations/2021_01_17_142804_create_biodatas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBiodatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('biodatas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('studentno');
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->string('lastname');
+            $table->string('course');
+            $table->string('address');
+            $table->string('gender');
+            $table->string('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
